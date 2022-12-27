8b1deb0ff435eef5ed80056bebd78e5416b1c41f
Yavdat <yavdat.it@gmail.com>
2022-12-27 22:28:59 +0300

    use  /usr/bin/env /bin/sh /tmp/Microsoft-MIEngine-Cmd-shmmfifz.1cx

--- TemplatesCPP.cpp
+++ TemplatesCPP.cpp
@@ -2,15 +2,21 @@
 
 using namespace std;
 
-int Sqr(int x) {
-    return x * x;
-}
+// int Sqr(int x) {
+    // return x * x;
+// }
 
-double Sqr(double x) {
+// double Sqr(double x) {
+    // return x * x;
+// }
+
+template <typename T>
+T Sqr(T x) {
     return x * x;
 }
 
 int main() {
-    cout << Sqr(2.5) << endl;
+    cout << Sqr(2.5) << endl;   // 6.25
+    cout << Sqr(2) << endl;     // 4
     return 0;
 }
