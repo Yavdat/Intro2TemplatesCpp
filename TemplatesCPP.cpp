1d027ef6ca49e17ae1ef1647c810900c054c2d49
Yavdat <yavdat.it@gmail.com>
2022-12-27 22:40:05 +0300

    overloaded operator* for pair and made pair p to test

--- TemplatesCPP.cpp
+++ TemplatesCPP.cpp
@@ -10,6 +10,13 @@ using namespace std;
     // return x * x;
 // }
 
+template <typename First, typename Second>
+pair<First, Second> operator * (const pair<First, Second>& p1, const pair<First, Second>& p2) {
+    First f = p1.first * p2.first;
+    Second s = p1.second * p2.second;
+    return make_pair(f, s);
+}
+
 template <typename T>
 T Sqr(T x) {
     return x * x;
@@ -18,5 +25,12 @@ T Sqr(T x) {
 int main() {
     cout << Sqr(2.5) << endl;   // 6.25
     cout << Sqr(2) << endl;     // 4
+
+    // возведение в квадрат пары
+    auto p = make_pair(2.5, 2);
+    auto res = Sqr(p);
+
+    cout << res.first << " " << res.second << endl; // то же самое, только в профиль
+
     return 0;
 }
